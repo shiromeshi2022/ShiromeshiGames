09ae8f7edb7e574c5b9abe3a9e00d36571b0b0e8
siromesi2022 <kansaifuming2000@gmail.com>
2020-08-29 14:33:13 +0900

    registerに利用規約を追加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,6 +7,7 @@
             <div class="card">
                 <div class="card-header">{{ __('ユーザー登録（無料）') }}</div>
 
+
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
@@ -24,6 +25,7 @@
                                 @enderror
                             </div>
                         </div>
+
 
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
@@ -48,9 +50,17 @@
                             </div>
                         </div>
 
+
+                        <div class="form-group mt-4 mx-auto text-md-center">
+                            <a href="{{route('service_term')}}"target="_blank">利用規約</a>
+                            <label for="agree" class="col-form-label text-md-right">に同意する</label>
+                            <input type="checkbox" id="agree" onClick="clickCheckBtn();" required/>
+                        </div>
+
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button id="submit" type="submit" class="btn btn-secondary" disabled>
                                     {{ __('登録する') }}
                                 </button>
                             </div>
@@ -61,4 +71,22 @@
         </div>
     </div>
 </div>
+
+
+<script>
+    const agree = document.getElementById('agree');
+    const submit = document.getElementById('submit');
+
+    function clickCheckBtn() {
+        submit.classList.toggle('btn-secondary');
+        submit.classList.toggle('btn-primary');
+        if(submit.disabled === true){
+            submit.disabled = false;
+        } else {
+            submit.disabled = true;
+        }
+    }
+</script>
+
+
 @endsection
